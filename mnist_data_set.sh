362b766354af6ac2c457e6ba29803cc33e82dc81
wisteria <1262098118@qq.com>
2018-08-02 20:42:48 +0800

    Update mnist_data_set.sh

--- mnist_data_set.sh
+++ mnist_data_set.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-mkdir -p /homt/jovyan/mnist_data_set
-wget -P /homt/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-wget -P /homt/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-wget -P /homt/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-wget -P /homt/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+mkdir -p /home/jovyan/mnist_data_set
+wget -P /home/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget -P /home/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget -P /home/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget -P /home/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
