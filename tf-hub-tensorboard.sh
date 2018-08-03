9c9b78023423c0a83ed33139492d7ba430a11369
wisteria <1262098118@qq.com>
2018-08-03 15:50:07 +0800

    Update tf-hub-tensorboard.sh

--- tf-hub-tensorboard.sh
+++ tf-hub-tensorboard.sh
@@ -8,6 +8,8 @@ set -xe
 uuid=`uuidgen`
 uuid=${uuid:0:8}
 
+NODE_HOST_IP=10.82.45.43
+
 CEPHFS_PV_YAML_TMP=/tmp/cephfs-pv-${uuid}.yaml
 TF_TENSORBOARD_YAML_TMP=/tmp/tf-tensorboard-${uuid}.yaml
 
@@ -21,18 +23,20 @@ sed -i "s/uuidgen/${uuid}/g" ${TF_TENSORBOARD_YAML_TMP}
 # cat ${CEPHFS_PV_YAML_TMP}
 # cat ${TF_TENSORBOARD_YAML_TMP}
 
-kubectl create -f ${CEPHFS_PV_YAML_TMP}
 kubectl create -f ${TF_TENSORBOARD_YAML_TMP}
+sleep 1
+kubectl create -f ${CEPHFS_PV_YAML_TMP}
 
+# see if the pv and pvc were created properly
 kubectl get pv
-
 kubectl get pvc
 
-while [ ! -d "/cephfs/jupyterhubpv/${uuid}-board" ]; do
+while [ ! -d "/cephfs/jupyterhubpv/${uuid}" ]; do
 	sleep 1
 done
 
-chmod -R 777 /cephfs/jupyterhubpv
+mkdir -p /cephfs/jupyterhubpv/${uuid}/work
+chmod -R 777 /cephfs/jupyterhubpv/${uuid}
 
 set +xe
 
@@ -44,5 +48,5 @@ JUPYTER_HUB_PORT=`kubectl get svc | grep tf-hub-lb | awk '{print $5}'`
 JUPYTER_HUB_PORT=${JUPYTER_HUB_PORT##*:}
 JUPYTER_HUB_PORT=${JUPYTER_HUB_PORT%/*}
 
-echo "Your jupyter_hub address: http://10.82.45.43:"$JUPYTER_HUB_PORT
-echo "Your tensorboard address: http://10.82.45.43:"$TENSORBOARD_PORT
+echo "Your jupyter_hub address: http://${NODE_HOST_IP}:"$JUPYTER_HUB_PORT
+echo "Your tensorboard address: http://${NODE_HOST_IP}:"$TENSORBOARD_PORT
