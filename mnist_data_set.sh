03c535b0a38fe7b6f93af0262b41bd67b6187eb8
wisteria <1262098118@qq.com>
2018-08-03 12:28:48 +0800

    Update mnist_data_set.sh

--- mnist_data_set.sh
+++ mnist_data_set.sh
@@ -3,7 +3,7 @@
 set -xe
 
 mkdir -p /home/jovyan/mnist_data_set
-wget -P /home/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-wget -P /home/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-wget -P /home/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-wget -P /home/jovyan/mnist_data_set http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+wget -P /home/jovyan/mnist_data_set http://pbqsx8kpd.bkt.clouddn.com/t10k-images-idx3-ubyte.gz
+wget -P /home/jovyan/mnist_data_set http://pbqsx8kpd.bkt.clouddn.com/t10k-labels-idx1-ubyte.gz
+wget -P /home/jovyan/mnist_data_set http://pbqsx8kpd.bkt.clouddn.com/train-images-idx3-ubyte.gz
+wget -P /home/jovyan/mnist_data_set http://pbqsx8kpd.bkt.clouddn.com/train-labels-idx1-ubyte.gz
